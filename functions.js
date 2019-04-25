fb1f59b35a615a82528ed3938ec50b1ccdb574cc
HazDuck <peter.e.heyes@gmail.com>
2019-04-25 19:45:23 +0100

    image links updated

--- functions.js
+++ functions.js
@@ -5,66 +5,66 @@ $.get('handy.hbs', function (data) {
 {
     "products": [
     {
-        "name": "Headphones",
-        "desc": "a pair of headphones",
+        "name": "Maecenas",
+        "desc": "Nullam eleifend mauris ultricies nulla porttitor",
         "price": 45.99,
         "originalPrice": 75,
         "discount": 45,
         "image": "https://cdn.shopify.com/s/files/1/1634/8185/products/akg_aviation_av100_anr_headset_01_270x335_crop_top.png?v=1480672940"
     },
     {
-        "name": "Coat",
-        "desc": "a awesome coat",
+        "name": "Morbi",
+        "desc": "Nullam tortor arcu varius natoque penatibu",
         "price": 120,
         "originalPrice": 120,
         "image": "https://cdn.shopify.com/s/files/1/1634/8185/products/alpha_industries_men_s_cwu_pilot_x_flight_jacket_01_270x335_crop_top.png?v=1480672952"
     },
     {
-        "name": "Sunglasses",
-        "desc": "swanky shades",
-        "price": 55,
+        "name": "Praesent id",
+        "desc": "Donec cursus sapien at dolor placerat, eget",
+        "price": 50,
         "originalPrice": 65,
         "discount": 15,
         "image": "https://cdn.shopify.com/s/files/1/1634/8185/products/american_optical_original_pilot_eyewear_57mm_frame_1_270x335_crop_top.png?v=1480672961"
     },
     {
-        "name": "Colour device",
-        "desc": "Not sure what this is?!",
-        "price": 150,
+        "name": "Vivamus volutpat a",
+        "desc": "Nullam eu consequat nibh. Orci varius natoque!",
+        "price": 154,
         "originalPrice": 160,
         "discount": 6,
         "image": "https://cdn.shopify.com/s/files/1/1634/8185/products/aspen_avionics_evolution_01_270x335_crop_top.png?v=1480672974"
     },
     {
-        "name": "Headphones",
-        "desc": "a pair of headphones",
+        "name": "Sodales",
+        "desc": "Ion maximus lobortis tellus quis egestas",
         "price": 45.99,
         "originalPrice": 75,
         "discount": 45,
-        "image": "https://cdn.shopify.com/s/files/1/1634/8185/products/akg_aviation_av100_anr_headset_01_270x335_crop_top.png?v=1480672940"
+        "image": "https://cdn.shopify.com/s/files/1/1634/8185/products/aviation_first_aid_kit_02_270x335_crop_top.png?v=1480672985"
     },
     {
-        "name": "Coat",
-        "desc": "a awesome coat",
-        "price": 120,
-        "originalPrice": 120,
-        "image": "https://cdn.shopify.com/s/files/1/1634/8185/products/alpha_industries_men_s_cwu_pilot_x_flight_jacket_01_270x335_crop_top.png?v=1480672952"
+        "name": "Vivamus volutpat a augue ",
+        "desc": "Ut libero odio, hendrerit in placerat non",
+        "price": 66,
+        "originalPrice": 66,
+        "image": "https:////cdn.shopify.com/s/files/1/1634/8185/products/cocoons_fitovers_polarized_sunglasses_pilot_lg_02_270x335_crop_top.png?v=1480673019"
     },
     {
-        "name": "Sunglasses",
-        "desc": "swanky shades",
-        "price": 55,
-        "originalPrice": 65,
+        "name": "Aliquam",
+        "desc": "Cras facilisis nibh vitaeid justo",
+        "price": 15,
+        "originalPrice": 30,
         "discount": 15,
-        "image": "https://cdn.shopify.com/s/files/1/1634/8185/products/american_optical_original_pilot_eyewear_57mm_frame_1_270x335_crop_top.png?v=1480672961"
+        "image": "https:////cdn.shopify.com/s/files/1/1634/8185/products/david_clark_dc_x11p_panel_anr_enc_aviation_pilot_headset_2_270x335_crop_top.png?v=1480673029"
     },
     {
-        "name": "Colour device",
-        "desc": "Not sure what this is?!",
-        "price": 150,
-        "originalPrice": 160,
+        "name": "Suspendisse",
+        "desc": "In maximus lobortis tellus quis egestas",
+        "price": 17,
+        "originalPrice": 23,
         "discount": 6,
-        "image": "https://cdn.shopify.com/s/files/1/1634/8185/products/aspen_avionics_evolution_01_270x335_crop_top.png?v=1480672974"
+        "image": "https:////cdn.shopify.com/s/files/1/1634/8185/products/garmin_aera_796_02_270x335_crop_top.png?v=1480673041"
     }
 ]
 }
